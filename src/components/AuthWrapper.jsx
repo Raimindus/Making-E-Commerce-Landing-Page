be8cc1717cf241be1f1c1b4abae7886f11b0edb0
irsyadfauzan <irsyadfauzan018@gmail.com>
2022-09-19 19:40:46 +0700

    authwrapper completed and working

--- src/components/AuthWrapper.jsx
+++ src/components/AuthWrapper.jsx
@@ -1,19 +1,22 @@
 import React, { useEffect } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { matchPath,useLocation, useNavigate } from 'react-router-dom';
 
 function AuthWrapper({ children }) {
   const navigate = useNavigate();
 
   const { pathname } = useLocation();
 
+  const matched = matchPath({ path: '/DetailSewa/:binarId' }, pathname);
+
   useEffect(() => {
-    if (pathname !== '/SignIn' && pathname !== '/SignUp' && pathname !== '/' && pathname !== '/CariMobil' && pathname !== '/DetailSewa/:binarId') {
+    console.log(matched);
+    if (pathname !== '/SignIn' && pathname !== '/SignUp' && pathname !== '/' && pathname !== '/CariMobil' && !matched) {
       const authData = JSON.parse(localStorage.getItem('auth'));
       if (!authData?.access_token) {
         navigate('/SignIn');
       }
     }
-  }, []);
+  }, [pathname]);
 
   return <div>{children}</div>;
 }
