616c4eab2b0bd08cc3703c203523d9692cb1c27b
irsyadfauzan <irsyadfauzan018@gmail.com>
2022-09-19 16:17:13 +0700

    g

--- src/services/tokenApi.js
+++ src/services/tokenApi.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
-export const tokenApi = axios.create({
+const tokenApi = axios.create({
   baseURL: 'https://bootcamp-rent-car.herokuapp.com',
   headers: {
     access_token: JSON.parse(localStorage.getItem('auth')).access_token,
   },
 });
+
+export default tokenApi;
